f34a544a7673baa566cce429b3c576efd9b8aa50
Jan-Willem de Bruyn <jan-willem@datm.nl>
2022-05-11 11:35:09 +0200

    Feature: cancellation support using /cancel?id=<>

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const lame = require('@suldashi/lame');
 const https = require('https');
 const Speaker = require('speaker');
 const jwt_decode = require('jwt-decode');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 const port = 3000
+const playLocks = {};
 
 app.get('/news', (req, res) => {
-  getNPORadio1News(3);
+  const uuid = getNPORadio1News(3);
 
-  res.send('Ok!')
+  res.send(uuid);
 })
+
+app.get('/cancel', (req, res) => {
+  const uuid = req.query.id;
+
+  // Acquire play pipe from array with uuid and close it
+  // This also cleans up the playlock
+  const request = playLocks[uuid];
+  request.close();
+  res.send('Ok!');
+});
 
 app.listen(port, () => {
   console.log(`audioplayer-news app listening on port ${port}`)
 })
 
 function getNPORadio1News(tryCounter) {
+  const uuid = uuidv4();
   https.get("https://www.nporadio1.nl/api/miniplayer/info?channel=npo-radio-1",
   function (response) {
     let data = '';
@@ -26,11 +39,13 @@ function getNPORadio1News(tryCounter) {
     response.on('data', (chunk) => data += chunk);
 
     // When all data is downloaded, send to decoder
-    response.on('end', () => decodeNPOMiniPlayer(data, tryCounter));
+    response.on('end', () => decodeNPOMiniPlayer(data, uuid, tryCounter));
   });
+
+  return uuid;
 }
 
-function decodeNPOMiniPlayer(miniplayerdata, tryCounter) {
+function decodeNPOMiniPlayer(miniplayerdata, uuid, tryCounter) {
   // Get location of embed code, this contains a JWT token
   const embedCode = JSON.parse(miniplayerdata).data.radio_news.player.embed_code;
   //Small regex to match the JWT token in the embed data
@@ -66,7 +81,17 @@ function decodeNPOMiniPlayer(miniplayerdata, tryCounter) {
       const newUrl = matches[0];
 
       // Get the MP3 file, send it to the lame decoder and pipe it to the speaker!
-      https.get(newUrl, (response) => response.pipe(new lame.Decoder).pipe(new Speaker));
+      https
+        .get(newUrl, (response) => {
+          const playPipe = response.pipe(new lame.Decoder).pipe(new Speaker);
+          // add the playback pipe to the playlocks for cancel support
+          playLocks[uuid] = playPipe;
+          
+          playPipe.on("close", () => {
+            delete playLocks[uuid];
+          })
+        } );
+        
     });
   });
 }
